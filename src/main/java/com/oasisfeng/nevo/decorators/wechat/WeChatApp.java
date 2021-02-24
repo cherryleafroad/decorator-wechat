596abd50322684b40fb84e4a2b4e749b1df8f224
Cherryleafroad <13651622+cherryleafroad@users.noreply.github.com>
2021-02-24 08:08:55 -0800

    Mark as read action button (can be disabled or enabled in settings)

--- src/main/java/com/oasisfeng/nevo/decorators/wechat/WeChatApp.java
+++ src/main/java/com/oasisfeng/nevo/decorators/wechat/WeChatApp.java
@@ -2,16 +2,20 @@ package com.oasisfeng.nevo.decorators.wechat;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 
 public class WeChatApp extends Application {
 
     private boolean isReplying = false;
+    private SharedPreferences preferences;
 
     public boolean getReplying() {
         return isReplying;
     }
+    public SharedPreferences getSharedPreferences() { return preferences; }
 
     public void setReplying(boolean state) {
         this.isReplying = state;
     }
+    public void setSharedPreferences(SharedPreferences pref) { this.preferences = pref; }
 }
